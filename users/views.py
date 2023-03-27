78fc51a7fa2765f54a0021c7793b49037b569cbb
mAnjra <39240839+mAnjra@users.noreply.github.com>
2023-03-28 00:36:44 +0100

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def register(request):
+    """Register a new user"""
+    if request.method != 'POST':
+        #Display blank form
+        form = UserCreationForm()
+    else:
+        #Process completed form.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            #Log the user in then - redirect to homepage - why its saved in variable
+
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    context = {'form' : form}
+    return render(request, 'registration/register.html', context)
